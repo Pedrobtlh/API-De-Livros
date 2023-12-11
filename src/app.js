ac4960540c553883178c43f672ade21d913fb8cd
Pedro Henrique Botelho <114683497+Pedrobtlh@users.noreply.github.com>
2023-12-11 19:40:42 -0300

    Adicionando MongoDB

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
 const livros = [
   {
@@ -13,6 +14,12 @@ const livros = [
   },
 ];
 
+function buscaLivro(id) {
+  return livros.findIndex((livros) => {
+    return livros.id === Number(id);
+  });
+}
+
 app.get("/", (req, res) => {
   res.status(200).send("Curso de Node.Js");
 });
@@ -21,4 +28,28 @@ app.get("/livros", (req, res) => {
   res.status(200).json(livros);
 });
 
+app.get("/livros/:id", (req, res) => {
+  const index = buscaLivro(req.params.id);
+  res.status(200).json(livros[index]);
+});
+
+app.post("/livros", (req, res) => {
+  livros.push(req.body);
+  res.status(201).send("Livro Cadastrado Com Sucesso!!");
+});
+
+app.put("/livros/:id", (req, res) => {
+  const index = buscaLivro(req.params.id);
+  livros[index].titulo = req.body.titulo;
+  res.status(200).json(livros);
+});
+
+app.delete("/livros/:id", (req, res) => {
+  const index = buscaLivro(req.params.id);
+  livros.splice(index, 1);
+  res.status(200).send("Livro Removido Com Sucesso!");
+});
+
 export default app;
+
+//mongodb+srv://admin:<password>@cluster0.ow8p5bp.mongodb.net/?retryWrites=true&w=majority
