4fca50241d30e197b61c1db6c9e7311472b584a7
Pedro Henrique Botelho <114683497+Pedrobtlh@users.noreply.github.com>
2023-12-27 19:33:28 -0300

    Adicionando Funções de Busca com Parametros

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { autorSchema } from "./Autor.js";
 
 const livroSchema = new mongoose.Schema(
   {
@@ -7,6 +8,7 @@ const livroSchema = new mongoose.Schema(
     editora: { type: String, required: true },
     preco: { type: Number },
     paginas: { type: Number },
+    autor: autorSchema,
   },
   { versionKey: false }
 );
